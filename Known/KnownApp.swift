23e2902fd45ac056c44fe4339d34fc00ec7229e4
Weijia Huang <hhuangweijia@gmail.com>
2026-01-10 23:09:50 -0500

    Initial Commit

---
+++ Known/KnownApp.swift
@@ -0,0 +1,32 @@
+//
+//  KnownApp.swift
+//  Known
+//
+//  Created by Weijia Huang on 1/10/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct KnownApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
